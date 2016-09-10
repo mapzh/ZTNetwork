999ef6ece80ed19501f8331187672d1cd9cd1e46
成林 <mpz129@sina.com>
2016-09-10 17:25:19 +0800

    service

---
+++ ZTNetwork/service/ZTNetworkService.h
@@ -0,0 +1,13 @@
+//
+//  ZTNetworkService.h
+//  ZTNetworkDemo
+//
+//  Created by mapengzhen on 16/9/10.
+//  Copyright © 2016年 mapengzhen. All rights reserved.
+//
+
+#import "ZTNetworkRequest.h"
+
+@interface ZTNetworkService : ZTNetworkRequest
+
+@end
---
+++ ZTNetwork/service/ZTNetworkService.m
@@ -0,0 +1,13 @@
+//
+//  ZTNetworkService.m
+//  ZTNetworkDemo
+//
+//  Created by mapengzhen on 16/9/10.
+//  Copyright © 2016年 mapengzhen. All rights reserved.
+//
+
+#import "ZTNetworkService.h"
+
+@implementation ZTNetworkService
+
+@end
